611854c1740591821c14547fd82c3da743517a9f
Roger David Alba Ortega <rogerdavidalbaortega@gmail.com>
2023-09-12 17:52:01 -0500

    Style: Add light to pageHome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
+'use client'
 import { Main } from '@/components/main/Main'
 import { Sidebar } from '@/components/sidebar/Sidebar'
 import '@/styles/pages/home.scss'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [infoPageHome, setInfoPageHome] = useState<HTMLElement | null>()
+  useEffect(() => {
+    const pageHome = document.getElementById('home')
+    setInfoPageHome(pageHome)
+  }, [])
   return (
-    <div className="home">
+    <div
+      id="home"
+      onMouseMove={(event) => {
+        const pageHome = infoPageHome
+        const { screenX, screenY } = event.nativeEvent
+        const styleLight = `radial-gradient(600px at ${screenX}px ${screenY}px, rgba(29, 78, 216, 0.15), transparent 80%)`
+        if (pageHome) {
+          pageHome.style.background = styleLight
+        }
+      }}
+    >
       <Sidebar />
       <Main />
+      <button className="shimmer-button">
+        <div className="light">
+          <div>button</div>
+        </div>
+      </button>
     </div>
   )
 }
